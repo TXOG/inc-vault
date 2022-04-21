d84fc371d48cfe23375b159554de35b92a36087d
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2022-04-21 13:40:04 +0100

    Add files via upload

---
+++ pipinstalls.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+echo installing bcrypt
+pip install bcrypt
+echo installing pathlib
+pip install pathlib
+echo installing cryptography
+pip install cryptography
+echo If you have anymore issues then create an issue on the github
+echo The program will continue in 5 seconds
