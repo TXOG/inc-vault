2d9308d3696e28bec29df9d5af4b06bbb1db2868
Thomas <TXOGFNAF@outlook.com>
2023-06-14 17:13:55 +0100

    Updated hashing to only use pyargon2 in the hashfile function

--- commands/security/hashfile.py
+++ commands/security/hashfile.py
@@ -1,11 +1,14 @@
 import hashlib
 from pyargon2 import hash
+import os
+import base64
 
 
 def hashfile(to_hash):
-    argonhash = str(hash(to_hash, "123456789123456789"))
+    argonhash = str(hash(to_hash, '23456345678346784567', hash_len=32))
     argonhash = argonhash.encode('utf-8')
-    return hashlib.sha256(argonhash).digest()
+    return argonhash[:32]
+
 
 
 def sha1_hash(to_hash):
